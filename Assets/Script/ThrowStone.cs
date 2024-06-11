eb5b8cc4220f5830c85a2ca8dfd5c5cd4b6da317
yusuke <nazuna10.yuyu@gmail.com>
2024-06-12 00:12:42 +0900

    update オセロ部分追加

---
+++ Assets/Script/ThrowStone.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ThrowStone : MonoBehaviour
+{
+    [SerializeField]
+    private StoneAndTarget.Type type = StoneAndTarget.Type.None;
+    [SerializeField]
+    private Transform _stone;
+    [SerializeField]
+    private bool _ones = false;
+    public bool Ones { get => _ones; set => _ones = value; }
+    public UnityAction ChangeTurnEvent { get; set; }
+
+    public void ChangeTurn(bool isBlackTurn)
+    {
+        Ones = false;
+        if (isBlackTurn)
+        {
+            type = StoneAndTarget.Type.BlackStone;
+            _stone.localRotation = Quaternion.Euler(new Vector3(StoneAndTarget.BLACK_STONE_X, 0, 0));
+        }
+        else
+        {
+            type = StoneAndTarget.Type.WhiteStone;
+            _stone.localRotation = Quaternion.Euler(new Vector3(StoneAndTarget.WHITE_STONE_X, 0, 0));
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.name.Equals("Target"))
+        {
+            var target = collider.transform.parent.GetComponent<StoneAndTarget>();
+            var list = target.GetImpactList(type);
+            if (list == null || list.Count == 0)
+                return;
+            //1ターンで置けるのは1回
+            Ones = true;
+            //接触した石を変更
+            target.Change(type);
+            //接触した石から波及して変更
+            foreach (var listItem in list)
+            {
+                listItem.Change(type);
+            }
+            ChangeTurnEvent.Invoke();
+        }
+    }
+}
